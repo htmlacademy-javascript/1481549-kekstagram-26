bff53c62bb940c2266e5836b6000e205381df359
alex <mrpatrickalex@gmail.com>
2022-07-16 08:57:40 +0300

    Добавил один общий обработчик для галереи, а не на каждую фотографию

--- js/photos.js
+++ js/photos.js
@@ -10,16 +10,31 @@ const picturesFragment = document.createDocumentFragment();
 const pictures = generatePhotos();
 
 pictures.forEach((picture) => {
-  const { url, likes, comments } = picture;
+  const { id, url, likes, comments } = picture;
   const pictureElement = pictureTemplate.cloneNode(true);
   pictureElement.querySelector('.picture__img').src = url;
   pictureElement.querySelector('.picture__comments').textContent =
     comments.length;
   pictureElement.querySelector('.picture__likes').textContent = likes;
-  pictureElement.addEventListener('click', () => {
-    createGallery(picture);
-  });
+  pictureElement.dataset.id = id;
+  // pictureElement.addEventListener('click', () => {
+  //   createGallery(picture);
+  // });
   picturesFragment.appendChild(pictureElement);
 });
 
 picturesElement.appendChild(picturesFragment);
+picturesElement.addEventListener('click', (event) => {
+  const [pictureElement] = event
+    .composedPath()
+    .filter((node) => node.classList && node.classList.contains('picture'));
+
+  if (!pictureElement) {
+    return;
+  }
+
+  const pictureId = Number(pictureElement.dataset.id);
+  const [picture] = pictures.filter(({ id }) => pictureId === id);
+
+  createGallery(picture);
+});
